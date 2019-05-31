c6b04129ed2882adebfe65f6182a4c88b9f340e1
Mackenzie Crawford <brickmack@gmail.com>
2019-05-31 18:26:18 -0400

    convert brk importer and init to tabs instead of spaces

--- release/scripts/addons/io_scene_obj/__init__.py
+++ release/scripts/addons/io_scene_obj/__init__.py
@@ -21,268 +21,268 @@
 # Derived from io_scene_obj in stock Blender, by Campbell Barton, Jiri Hnidek, and Paolo Ciccone
 
 bl_info = {
-    "name": "BrickCAD BRK format",
-    "author": "",
-    "version": (1, 0, 0),
-    "blender": (2, 80, 0),
-    "location": "File > Import-Export",
-    "description": "Import-Export BRK, Import BRK mesh, UV's, materials and textures",
-    "warning": "",
-    "wiki_url": "",
-    "support": 'OFFICIAL',
-    "category": "Import-Export"}
+	"name": "BrickCAD BRK format",
+	"author": "",
+	"version": (1, 0, 0),
+	"blender": (2, 80, 0),
+	"location": "File > Import-Export",
+	"description": "Import-Export BRK, Import BRK mesh, UV's, materials and textures",
+	"warning": "",
+	"wiki_url": "",
+	"support": 'OFFICIAL',
+	"category": "Import-Export"}
 
 if "bpy" in locals():
-    import importlib
-    if "import_brk" in locals():
-        importlib.reload(import_brk)
-    if "export_brk" in locals():
-        importlib.reload(export_brk)
+	import importlib
+	if "import_brk" in locals():
+		importlib.reload(import_brk)
+	if "export_brk" in locals():
+		importlib.reload(export_brk)
 
 
 import bpy
 from bpy.props import (
-        BoolProperty,
-        FloatProperty,
-        StringProperty,
-        EnumProperty,
-        )
+		BoolProperty,
+		FloatProperty,
+		StringProperty,
+		EnumProperty,
+		)
 from bpy_extras.io_utils import (
-        ImportHelper,
-        ExportHelper,
-        orientation_helper,
-        path_reference_mode,
-        axis_conversion,
-        )
+		ImportHelper,
+		ExportHelper,
+		orientation_helper,
+		path_reference_mode,
+		axis_conversion,
+		)
 
 @orientation_helper(axis_forward='-Z', axis_up='Y')
 class ImportBRK(bpy.types.Operator, ImportHelper):
-    """Load a BrickCAD BRK File"""
-    bl_idname = "import_scene.obj"
-    bl_label = "Import BRK"
-    bl_options = {'PRESET', 'UNDO'}
+	"""Load a BrickCAD BRK File"""
+	bl_idname = "import_scene.obj"
+	bl_label = "Import BRK"
+	bl_options = {'PRESET', 'UNDO'}
 
-    filename_ext = ".brk"
-    filter_glob: StringProperty(
-            default="*.brk",
-            options={'HIDDEN'},
-            )
+	filename_ext = ".brk"
+	filter_glob: StringProperty(
+			default="*.brk",
+			options={'HIDDEN'},
+			)
 
-    use_edges: BoolProperty(
-            name="Lines",
-            description="Import lines and faces with 2 verts as edge",
-            default=True,
-            )
-    use_smooth_groups: BoolProperty(
-            name="Smooth Groups",
-            description="Surround smooth groups by sharp edges",
-            default=True,
-            )
+	use_edges: BoolProperty(
+			name="Lines",
+			description="Import lines and faces with 2 verts as edge",
+			default=True,
+			)
+	use_smooth_groups: BoolProperty(
+			name="Smooth Groups",
+			description="Surround smooth groups by sharp edges",
+			default=True,
+			)
 
-    use_split_objects: BoolProperty(
-            name="Object",
-            description="Import BRK Objects into Blender Objects",
-            default=True,
-            )
-    use_split_groups: BoolProperty(
-            name="Group",
-            description="Import BRK Groups into Blender Objects",
-            default=False,
-            )
+	use_split_objects: BoolProperty(
+			name="Object",
+			description="Import BRK Objects into Blender Objects",
+			default=True,
+			)
+	use_split_groups: BoolProperty(
+			name="Group",
+			description="Import BRK Groups into Blender Objects",
+			default=False,
+			)
 
-    split_mode: EnumProperty(
-            name="Split",
-            items=(('ON', "Split", "Split geometry, omits unused verts"),
-                   ('OFF', "Keep Vert Order", "Keep vertex order from file"),
-                   ),
-            )
+	split_mode: EnumProperty(
+			name="Split",
+			items=(('ON', "Split", "Split geometry, omits unused verts"),
+				   ('OFF', "Keep Vert Order", "Keep vertex order from file"),
+				   ),
+			)
 
-    global_clight_size: FloatProperty(
-            name="Clamp Size",
-            description="Clamp bounds under this value (zero to disable)",
-            min=0.0, max=1000.0,
-            soft_min=0.0, soft_max=1000.0,
-            default=0.0,
-            )
+	global_clight_size: FloatProperty(
+			name="Clamp Size",
+			description="Clamp bounds under this value (zero to disable)",
+			min=0.0, max=1000.0,
+			soft_min=0.0, soft_max=1000.0,
+			default=0.0,
+			)
 
-    def execute(self, context):
-        # print("Selected: " + context.active_object.name)
-        from . import import_brk
+	def execute(self, context):
+		# print("Selected: " + context.active_object.name)
+		from . import import_brk
 
-        if self.split_mode == 'OFF':
-            self.use_split_objects = False
-            self.use_split_groups = False
-        else:
-            self.use_groups_as_vgroups = False
+		if self.split_mode == 'OFF':
+			self.use_split_objects = False
+			self.use_split_groups = False
+		else:
+			self.use_groups_as_vgroups = False
 
-        keywords = self.as_keywords(ignore=("axis_forward",
-                                            "axis_up",
-                                            "filter_glob",
-                                            "split_mode",
-                                            ))
+		keywords = self.as_keywords(ignore=("axis_forward",
+											"axis_up",
+											"filter_glob",
+											"split_mode",
+											))
 
-        global_matrix = axis_conversion(from_forward=self.axis_forward,
-                                        from_up=self.axis_up,
-                                        ).to_4x4()
-        keywords["global_matrix"] = global_matrix
+		global_matrix = axis_conversion(from_forward=self.axis_forward,
+										from_up=self.axis_up,
+										).to_4x4()
+		keywords["global_matrix"] = global_matrix
 
-        if bpy.data.is_saved and context.preferences.filepaths.use_relative_paths:
-            import os
-            keywords["relpath"] = os.path.dirname(bpy.data.filepath)
+		if bpy.data.is_saved and context.preferences.filepaths.use_relative_paths:
+			import os
+			keywords["relpath"] = os.path.dirname(bpy.data.filepath)
 
-        return import_brk.load(context, **keywords)
+		return import_brk.load(context, **keywords)
 
-    def draw(self, context):
-        layout = self.layout
+	def draw(self, context):
+		layout = self.layout
 
-        row = layout.row(align=True)
-        row.prop(self, "use_smooth_groups")
-        row.prop(self, "use_edges")
+		row = layout.row(align=True)
+		row.prop(self, "use_smooth_groups")
+		row.prop(self, "use_edges")
 
-        box = layout.box()
-        row = box.row()
-        row.prop(self, "split_mode", expand=True)
+		box = layout.box()
+		row = box.row()
+		row.prop(self, "split_mode", expand=True)
 
-        row = box.row()
-        if self.split_mode == 'ON':
-            row.label(text="Split by:")
-            row.prop(self, "use_split_objects")
-            row.prop(self, "use_split_groups")
-        else:
-            row.prop(self, "use_groups_as_vgroups")
+		row = box.row()
+		if self.split_mode == 'ON':
+			row.label(text="Split by:")
+			row.prop(self, "use_split_objects")
+			row.prop(self, "use_split_groups")
+		else:
+			row.prop(self, "use_groups_as_vgroups")
 
-        row = layout.split(factor=0.67)
-        row.prop(self, "global_clight_size")
-        layout.prop(self, "axis_forward")
-        layout.prop(self, "axis_up")
+		row = layout.split(factor=0.67)
+		row.prop(self, "global_clight_size")
+		layout.prop(self, "axis_forward")
+		layout.prop(self, "axis_up")
 
-        layout.prop(self, "use_image_search")
+		layout.prop(self, "use_image_search")
 
 
 @orientation_helper(axis_forward='-Z', axis_up='Y')
 class ExportBRK(bpy.types.Operator, ExportHelper):
-    """Save a BrickCAD BRK File"""
+	"""Save a BrickCAD BRK File"""
 
-    bl_idname = "export_scene.obj"
-    bl_label = 'Export BRK'
-    bl_options = {'PRESET'}
+	bl_idname = "export_scene.obj"
+	bl_label = 'Export BRK'
+	bl_options = {'PRESET'}
 
-    filename_ext = ".brk"
-    filter_glob: StringProperty(
-            default="*.brk",
-            options={'HIDDEN'},
-            )
+	filename_ext = ".brk"
+	filter_glob: StringProperty(
+			default="*.brk",
+			options={'HIDDEN'},
+			)
 
-    # context group
-    use_selection: BoolProperty(
-            name="Selection Only",
-            description="Export selected objects only",
-            default=False,
-            )
+	# context group
+	use_selection: BoolProperty(
+			name="Selection Only",
+			description="Export selected objects only",
+			default=False,
+			)
 
-    # extra data group
-    use_edges: BoolProperty(
-            name="Include Edges",
-            description="",
-            default=True,
-            )
-    use_smooth_groups: BoolProperty(
-            name="Smooth Groups",
-            description="Write sharp edges as smooth groups",
-            default=False,
-            )
-    use_smooth_groups_bitflags: BoolProperty(
-            name="Bitflag Smooth Groups",
-            description="Same as 'Smooth Groups', but generate smooth groups IDs as bitflags "
-                        "(produces at most 32 different smooth groups, usually much less)",
-            default=False,
-            )
-    use_normals: BoolProperty(
-            name="Write Normals",
-            description="Export one normal per vertex and per face, to represent flat faces and sharp edges",
-            default=True,
-            )
-    use_uvs: BoolProperty(
-            name="Include UVs",
-            description="Write out the active UV coordinates",
-            default=True,
-            )
-    use_triangles: BoolProperty(
-            name="Triangulate Faces",
-            description="Convert all faces to triangles",
-            default=False,
-            )
-    use_vertex_groups: BoolProperty(
-            name="Polygroups",
-            description="",
-            default=False,
-            )
+	# extra data group
+	use_edges: BoolProperty(
+			name="Include Edges",
+			description="",
+			default=True,
+			)
+	use_smooth_groups: BoolProperty(
+			name="Smooth Groups",
+			description="Write sharp edges as smooth groups",
+			default=False,
+			)
+	use_smooth_groups_bitflags: BoolProperty(
+			name="Bitflag Smooth Groups",
+			description="Same as 'Smooth Groups', but generate smooth groups IDs as bitflags "
+						"(produces at most 32 different smooth groups, usually much less)",
+			default=False,
+			)
+	use_normals: BoolProperty(
+			name="Write Normals",
+			description="Export one normal per vertex and per face, to represent flat faces and sharp edges",
+			default=True,
+			)
+	use_uvs: BoolProperty(
+			name="Include UVs",
+			description="Write out the active UV coordinates",
+			default=True,
+			)
+	use_triangles: BoolProperty(
+			name="Triangulate Faces",
+			description="Convert all faces to triangles",
+			default=False,
+			)
+	use_vertex_groups: BoolProperty(
+			name="Polygroups",
+			description="",
+			default=False,
+			)
 
-    keep_vertex_order: BoolProperty(
-            name="Keep Vertex Order",
-            description="",
-            default=False,
-            )
+	keep_vertex_order: BoolProperty(
+			name="Keep Vertex Order",
+			description="",
+			default=False,
+			)
 
-    global_scale: FloatProperty(
-            name="Scale",
-            min=0.01, max=1000.0,
-            default=1.0,
-            )
+	global_scale: FloatProperty(
+			name="Scale",
+			min=0.01, max=1000.0,
+			default=1.0,
+			)
 
-    path_mode: path_reference_mode
+	path_mode: path_reference_mode
 
-    check_extension = True
+	check_extension = True
 
-    def execute(self, context):
-        from . import export_brk
+	def execute(self, context):
+		from . import export_brk
 
-        from mathutils import Matrix
-        keywords = self.as_keywords(ignore=("axis_forward",
-                                            "axis_up",
-                                            "global_scale",
-                                            "check_existing",
-                                            "filter_glob",
-                                            ))
+		from mathutils import Matrix
+		keywords = self.as_keywords(ignore=("axis_forward",
+											"axis_up",
+											"global_scale",
+											"check_existing",
+											"filter_glob",
+											))
 
-        global_matrix = (Matrix.Scale(self.global_scale, 4) @
-                         axis_conversion(to_forward=self.axis_forward,
-                                         to_up=self.axis_up,
-                                         ).to_4x4())
+		global_matrix = (Matrix.Scale(self.global_scale, 4) @
+						 axis_conversion(to_forward=self.axis_forward,
+										 to_up=self.axis_up,
+										 ).to_4x4())
 
-        keywords["global_matrix"] = global_matrix
-        return export_brk.save(context, **keywords)
+		keywords["global_matrix"] = global_matrix
+		return export_brk.save(context, **keywords)
 
 
 def menu_func_import(self, context):
-    self.layout.operator(ImportBRK.bl_idname, text="Brick (.brk)")
+	self.layout.operator(ImportBRK.bl_idname, text="Brick (.brk)")
 
 
 def menu_func_export(self, context):
-    self.layout.operator(ExportBRK.bl_idname, text="Brick (.brk)")
+	self.layout.operator(ExportBRK.bl_idname, text="Brick (.brk)")
 
 
 classes = (
-    ImportBRK,
-    ExportBRK,
+	ImportBRK,
+	ExportBRK,
 )
 
 
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
+	for cls in classes:
+		bpy.utils.register_class(cls)
 
-    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
-    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+	bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+	bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
-    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+	bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+	bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
 
 
 if __name__ == "__main__":
-    register()
+	register()
